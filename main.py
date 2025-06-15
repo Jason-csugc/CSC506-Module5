ef6cf4547d9c6fe2d6ffc123c18f08908c6e33d7
tinidian <tinidian@gmail.com>
2025-06-15 15:01:21 -0600

    Minor updates to display

--- main.py
+++ main.py
@@ -8,11 +8,17 @@ def main():
 
     hash = Hash(BUCKET_SIZE)
 
-    arr = random.sample(range(BUCKET_SIZE * 10), BUCKET_SIZE * 10)
-    print(arr)
+    arr = random.sample(range(BUCKET_SIZE * 20), BUCKET_SIZE * 10)
     for x in arr:
         hash.insertItem(x)
+
+    print(f"Array (total elements: {len(arr)}):")
+
+    for i in range(10):
+        print(f"elements {i*10} - {i * 10 + 10}: {arr[i * 10: i * 10 + 10]}")
     
+    print("")
+    print("Hash Table:")
     hash.displayHash()
 
 if __name__ == "__main__":
